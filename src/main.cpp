69dd9cc5650d2e011c75dbfb57fe6a830a1af990
Kai Lawrence <kai.lawrence@t-online.de>
2017-06-12 17:56:54 +0000

    REALLIGNING OF OUTPUT AND ATTEMPT TO DIFFERENCIATE

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,37 @@
 
 int main()
 {
+	using namespace std::chrono;
+	using namespace std::literals::chrono_literals;
+
 	wiringPiSetupGpio();
 	mpu6050 sensor { device_id { 0b1101000 } };
 	
-	StepperDriver driver; 
+	StepperDriver driver;
 
-	for (int i = 0; i < 2000; ++i)
+	double deg = 0;
+	double gyro_z = 0;
+	
+	auto start_time = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 10000; ++i)
 	{	
 		driver.FullStep();
-		std::this_thread::sleep_for(std::chrono::milliseconds(2));
 
 		sensor.update();
-		auto acc = sensor.acceleration();
-		printf("(%d, %d, %d)\n", acc.x, acc.y, acc.z);
-		std::cout << sensor.gyro().z << std::endl;
+		//auto acc = sensor.acceleration();
+		//printf("(%d, %d, %d)\n", acc.x, acc.y, acc.z);
+		
+		double gyro_z_old = gyro_z;
+		double gyro_z = sensor.gyro().z * 250. /32768 - 0.512;
+		std::this_thread::sleep_for(2939us - (high_resolution_clock::now() - start_time));
+		auto elapsed_time = duration_cast<microseconds>(high_resolution_clock::now() - start_time);
+		start_time = high_resolution_clock::now();
+		deg += elapsed_time.count() * gyro_z / 1'000'000;
+		double accel = (gyro_z - gyro_z_old)*elapsed_time.count()/1'000'000;
+
+		std::cout.precision(2);
+		std::cout << accel << std::fixed << std::showpos << "\tdeg/s2 | " << gyro_z << std::fixed << std::showpos << "\tdeg/s | " << deg << std::fixed << std::showpos << "\tdeg | "
+		       	  << elapsed_time.count() << " microseconds" << std::endl;
 	}
 	driver.Disable();
 }
-
--- src/stepper.cpp
+++ src/stepper.cpp
@@ -4,19 +4,19 @@
 #include <thread>
 #include <wiringPi.h>
 
-StepperDriver::StepperDriver() // StepperDriver() bekommt diese 4 Pins als default
+StepperDriver::StepperDriver() 
 	: StepperDriver(238, 236, 233, 231)
 {
 }
 
-StepperDriver::StepperDriver(int a, int b, int c, int d) // Diese 4 default Pins werden hier als Array gespeichert.
+StepperDriver::StepperDriver(int a, int b, int c, int d) 
 	: mPin{a, b, c, d}
 	, mPosition(0)
 {
 	for (int i = 0; i < 4; ++i)
 	{
-		pinMode(mPin[i], 1); // Die 4 Pins werden als aktiv gesetzt
-		digitalWrite(mPin[i], 0); // Die 4 Pins werden auf 0/low gesetzt
+		pinMode(mPin[i], 1); 
+		digitalWrite(mPin[i], 0); 
 	}
 	UpdatePositionSignal(true);
 }
@@ -26,13 +26,13 @@ StepperDriver::~StepperDriver()
 	Disable();
 }
 
-void StepperDriver::AdvancePositionBy(int num) // bestimmt die naechste Position des Motots
+void StepperDriver::AdvancePositionBy(int num)
 {
 	UpdatePositionSignal(false);
 
-	int posCandidate = mPosition + num; // num ist entweder 1 oder -1 (Forward oder Backward)
-	int sign = posCandidate < 0; //wenn sign negativ ist:
-	mPosition = (sign * -posCandidate + !sign * posCandidate) % 8; //wird mPosition positiv, sonst bleibt er positiv. das wird % 8 gerechnet, sodass mPosition zwischen 0 und 7 ist
+	int posCandidate = mPosition + num; 
+	int sign = posCandidate < 0;
+	mPosition = (sign * -posCandidate + !sign * posCandidate) % 8; 
 	
 	UpdatePositionSignal(true);
 }
@@ -52,13 +52,13 @@ void StepperDriver::Disable()
 	UpdatePositionSignal(false);
 }
 
-void StepperDriver::HalfStep(Direction dir) //definition der Halfstep Funktion. uebergeben wird die richtung, welche standardmaessig forwards ist
+void StepperDriver::HalfStep(Direction dir) 
 {
-	AdvancePositionBy(static_cast<int>(dir)); //setzt die position des motors einen schritt weiter
+	AdvancePositionBy(static_cast<int>(dir)); 
 }
 
-void StepperDriver::FullStep(Direction dir) // das gleiche nochmal fuer fullsteps
+void StepperDriver::FullStep(Direction dir) 
 {
 	auto advance = static_cast<int>(dir);
-	AdvancePositionBy(advance * (!(mPosition % 2) + 1)); // falls die momentane position ein halfstep ist, wird auf den naechsten fullstep gesetzt
+	AdvancePositionBy(advance * (!(mPosition % 2) + 1));
 }
